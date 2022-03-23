0ecbe5e82b69b2205ea7a9491135bf5fd8112647
Charlie M <cmathIV@protonmail.com>
2022-03-22 19:40:24 -0500

    Initial commit

---
+++ app/api/routes/routing.ts
@@ -0,0 +1,29 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes.
+*/
+
+import Route from "@ioc:Adonis/Core/Route";
+import HealthCheck from "@ioc:Adonis/Core/HealthCheck";
+
+Route.group(() => {
+  //Health check
+  //Make sure to configure .env values
+  Route.get("health", async ({ response }) => {
+    const report = await HealthCheck.getReport();
+
+    return report.healthy ? response.ok(report) : response.badRequest(report);
+  });
+
+  //Test
+  Route.get("test", async ({ response }) => {
+    response.json({
+      message: "ok",
+    });
+
+    return response.status(200);
+  });
+}).prefix("/api");
